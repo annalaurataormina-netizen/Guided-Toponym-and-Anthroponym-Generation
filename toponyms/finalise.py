ddc127258dbdc82faaa1482cc7dea0356fb0560f
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-06-16 15:41:04 +0100

    Improved the finalise.py script for toponyms.

--- toponyms/finalise.py
+++ toponyms/finalise.py
@@ -1,9 +1,22 @@
 import gzip
 import json
+from collections import Counter
+
+from icu import Transliterator, defaultdict
 
 
 def main():
+    transliterator = Transliterator.createInstance("Any-Latin")
     counter = 0
+
+    character_counter = Counter()
+
+    breakdown_by_language = Counter()
+    breakdown_by_country = Counter()
+    breakdown_by_length = Counter()
+
+    length_by_country = defaultdict(Counter)
+    length_by_language = defaultdict(Counter)
 
     with gzip.open('/vol/bitbucket/at2225/toponyms_cleaned.jsonl.gz', 'rt') as input:
         with gzip.open('/vol/bitbucket/at2225/toponyms_finalised.jsonl.gz', 'wt') as output:
@@ -15,11 +28,14 @@ def main():
                 if not entity['name']:
                     continue
 
-                for language in entity['name'].keys():
+                for language, name in entity['name']:
+                    name_romanised = transliterator.transliterate(name['name'])
+
                     toponym = {
-                        'name': entity['name'][language]['name'],
+                        'name': name_romanised,
+                        'name_original': name['name'],
                         'language': language,
-                        'language_code': entity['name'][language]['code'],
+                        'language_code': name['code'],
                         'id': entity['id'],
                         'type': entity['type'],
                         'country': entity['country'],
@@ -29,8 +45,25 @@ def main():
 
                     counter += 1
 
-    print('# of toponyms: ', counter)
+                    character_counter.update(name_romanised)
 
+                    breakdown_by_language[language] += 1
+
+                    for country in entity['country']:
+                        breakdown_by_country[country] += 1
+
+                    breakdown_by_length[len(name_romanised)] += 1
+
+                    length_by_country[country][len(name_romanised)] += 1
+                    length_by_language[language][len(name_romanised)] += 1
+
+    print('# of toponyms: ', counter)
+    print('Character occurrences: ', character_counter)
+    print('Breakdown by language: ', breakdown_by_language)
+    print('Breakdown by country: ', breakdown_by_country)
+    print('Breakdown by length: ', breakdown_by_length)
+    print('Length by country: ', length_by_country)
+    print('Length by language: ', length_by_language)
 
 if __name__ == '__main__':
     main()
