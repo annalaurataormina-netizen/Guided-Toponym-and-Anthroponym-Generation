f6241d22f0366c1346127305215310d49070f6d7
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-06-17 11:57:07 +0100

    Completed the finalise.py script for toponyms. Almost done with toponyms, just need to clean up etc etc

--- toponyms/check.py
+++ toponyms/check.py
@@ -1,12 +1,13 @@
 import gzip
 import json
 import random
+from collections import Counter
 
 
 def main():
-    ids = []
-    types = {}
-    countries = {}
+    entities_missing_name = []
+    types_missing_name = Counter()
+    countries_missing_name = Counter()
 
     with gzip.open('/vol/bitbucket/at2225/toponyms_cleaned.jsonl.gz', 'rt') as input:
 
@@ -15,23 +16,16 @@ def main():
             entity = json.loads(line)
 
             if not entity.get('name'):
-                ids.append(entity.get('id'))
-                for type in entity.get('type'):
-                    if type in types.keys():
-                        types[type] += 1
-                    else:
-                        types[type] = 1
-                if entity.get('country'):
-                    for country in entity.get('country'):
-                        if country not in countries.keys():
-                            countries[country] = 1
-                        else:
-                            countries[country] += 1
+                entities_missing_name.append(entity.get('id'))
 
-    print('# missing name: ', len(ids))
-    print('First 100 IDs (shuffled list): ', random.sample(ids, 100))
-    print('breakdown by type: ', types)
-    print('breakdown by country: ', countries)
+                types_missing_name.update(entity.get('type', []))
+                countries_missing_name.update(entity.get('country', []))
+
+    print('FOR ENTITIES MISSING NAMES')
+    print('#: ', len(entities_missing_name))
+    print('100 IDs (shuffled): ', random.sample(entities_missing_name, min(100, len(entities_missing_name))))
+    print('breakdown by type: ', types_missing_name)
+    print('breakdown by country: ', countries_missing_name)
 
 
 if __name__ == '__main__':
--- toponyms/finalise.py
+++ toponyms/finalise.py
@@ -1,8 +1,8 @@
 import gzip
 import json
-from collections import Counter
+from collections import Counter, defaultdict
 
-from icu import Transliterator, defaultdict
+from icu import Transliterator
 
 
 def main():
@@ -10,30 +10,38 @@ def main():
     counter = 0
 
     character_counter = Counter()
+    character_counter_romanised = Counter()
 
     breakdown_by_language = Counter()
     breakdown_by_country = Counter()
     breakdown_by_length = Counter()
+    breakdown_by_length_romanised = Counter()
 
     length_by_country = defaultdict(Counter)
     length_by_language = defaultdict(Counter)
 
+    length_by_country_romanised = defaultdict(Counter)
+    length_by_language_romanised = defaultdict(Counter)
+
     with gzip.open('/vol/bitbucket/at2225/toponyms_cleaned.jsonl.gz', 'rt') as input:
-        with gzip.open('/vol/bitbucket/at2225/toponyms_finalised.jsonl.gz', 'wt') as output:
+        with gzip.open('/vol/bitbucket/at2225/toponyms_final.jsonl.gz', 'wt') as output:
 
             for line in input:
 
                 entity = json.loads(line)
 
+                # Skip entities without names.
                 if not entity['name']:
                     continue
 
-                for language, name in entity['name']:
+                for language, name in entity['name'].items():
+
+                    # Get the romanised version of the name.
                     name_romanised = transliterator.transliterate(name['name'])
 
                     toponym = {
-                        'name': name_romanised,
-                        'name_original': name['name'],
+                        'name_romanised': name_romanised,
+                        'name': name['name'],
                         'language': language,
                         'language_code': name['code'],
                         'id': entity['id'],
@@ -45,25 +53,35 @@ def main():
 
                     counter += 1
 
-                    character_counter.update(name_romanised)
+                    character_counter.update(name['name'])
+                    character_counter_romanised.update(name_romanised)
 
-                    breakdown_by_language[language] += 1
+                    breakdown_by_language.update([language])
+
+                    breakdown_by_length[len(name['name'])] += 1
+                    breakdown_by_length_romanised[len(name_romanised)] += 1
 
                     for country in entity['country']:
-                        breakdown_by_country[country] += 1
+                        breakdown_by_country.update([country])
 
-                    breakdown_by_length[len(name_romanised)] += 1
+                        length_by_country[country][len(name['name'])] += 1
+                        length_by_country_romanised[country][len(name_romanised)] += 1
 
-                    length_by_country[country][len(name_romanised)] += 1
-                    length_by_language[language][len(name_romanised)] += 1
+                    length_by_language[language][len(name['name'])] += 1
+                    length_by_language_romanised[language][len(name_romanised)] += 1
 
     print('# of toponyms: ', counter)
     print('Character occurrences: ', character_counter)
+    print('Character occurrences in romanised names: ', character_counter_romanised)
     print('Breakdown by language: ', breakdown_by_language)
     print('Breakdown by country: ', breakdown_by_country)
     print('Breakdown by length: ', breakdown_by_length)
+    print('Breakdown by length (romanised): ', breakdown_by_length_romanised)
     print('Length by country: ', length_by_country)
     print('Length by language: ', length_by_language)
+    print('Length by country (romanised): ', length_by_country_romanised)
+    print('Length by language (romanised): ', length_by_language_romanised)
+
 
 if __name__ == '__main__':
     main()
