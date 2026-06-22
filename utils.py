ac8e2c228f9cb161de4c0d7e4d6cd69f1724f9f7
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-06-22 19:25:00 +0100

    Made further changes to the finalise script.

--- utils.py
+++ utils.py
@@ -255,7 +255,7 @@ def get_romanised(name):
 
 
 def split_diacritics(name: str) -> str:
-    return ''.join(unicodedata.normalize('NFD', char) for char in name)
+    return unicodedata.normalize('NFD', name)
 
 
 def get_countries_names(ids: list) -> dict[str, str]:
