f1db572afe41a46a27e818edb4030047823ff641
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-06-25 11:14:54 +0100

    Made final changes to finalise.py

--- anthroponyms/finalise.py
+++ anthroponyms/finalise.py
@@ -9,7 +9,7 @@ from langcodes import Language
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils import get_romanised, get_countries_names, get_country_languages
 
-from clean import OCCURRENCE_THRESHOLD
+from clean import OCCURRENCE_THRESHOLD, OCCURRENCE_THRESHOLD_CITIZENSHIP
 
 MIN_LENGTH_THRESHOLD = 2
 MAX_LENGTH_THRESHOLD = 20
@@ -43,120 +43,6 @@ EXCLUDE_LANGUAGES = [
     "Ancient Greek",
     "Ottoman Turkish",
     "Prussian",
-
-    # Language families
-    "Bihari languages",
-    "Nahuatl languages",
-
-    # Writing system
-    "N'Ko",
-    "N’Ko",
-
-    # Small (<20 anthroponyms)
-    "Ewe",
-    "Dzongkha",
-    "Filipino",
-    "Corsican",
-    "Asturian",
-    "Chechen",
-    "Atikamekw",
-    "Sardinian",
-    "Fang",
-    "Ga",
-    "Latgalian",
-    "Narom",
-    "Frafra",
-    "Hawaiian",
-    "Akan",
-    "Fanti",
-    "Moroccan Arabic",
-    "Balinese",
-    "Abkhazian",
-    "Pampanga",
-    "Arpitan",
-    "Lower Sorbian",
-    "Egyptian Arabic",
-    "Western Panjabi",
-    "Navajo",
-    "Min Nan Chinese",
-    "Ligurian",
-    "Low German",
-    "Adyghe",
-    "Erzya",
-    "Friulian",
-    "Dagbani",
-    "Silesian",
-    "Basaa",
-    "Tahitian",
-    "Algerian Arabic",
-    "Kinaray-a",
-    "Sicilian",
-    "Bavarian",
-    "Kumyk",
-    "Venetian",
-    "Mapuche",
-    "Kalaallisut",
-    "Cebuano",
-    "Javanese",
-    "Lezghian",
-    "Tuvinian",
-    "Manx",
-    "Avaric",
-    "Cajun French",
-    "Picard",
-    "Batak Mandailing",
-    "Skolt Sami",
-    "Sranan Tongo",
-    "Bini",
-    "Fiji Hindi",
-    "Northern Frisian",
-    "Baoulé",
-    "Saterland Frisian",
-    "Ibibio",
-    "Efik",
-    "Votic",
-    "Central Bikol",
-    "Saraiki",
-    "Jamaican Creole English",
-    "Waray",
-    "Cantonese",
-    "Kashmiri",
-    "Acehnese",
-    "Iloko",
-    "Eastern Mari",
-    "Lower Silesian",
-    "Karachay-Balkar",
-    "Rusyn",
-    "Northern Sotho",
-    "Nigerian Pidgin",
-    "Buginese",
-    "Southern Dagaare",
-    "Uyghur",
-    "Aramaic",
-    "Ganda",
-    "Yakut",
-    "Limburgish",
-    "Tiv",
-    "Hiligaynon",
-    "Newari",
-    "Plautdietsch",
-    "Sundanese",
-    "Tibetan",
-    "Pangasinan",
-    "Tachelhit",
-    "Papiamento",
-    "Aromanian",
-    "South Azerbaijani",
-    "Bodo",
-    "Karelian",
-    "Talysh",
-    "Veps",
-    "Inari Sami",
-    "Komi",
-    "Goan Konkani",
-    "Tornedalen Finnish",
-    "Tuvalu",
-    "Standard Moroccan Tamazight",
 ]
 
 
@@ -214,55 +100,34 @@ def main():
                            and c in COUNTRY_LANGUAGES
                     ]
 
-                    languages_countries_of_birth = []
+                    countries_of_citizenship = entity.get('occurrences', {}).get('country_of_citizenship', {})
+
+                    for country in countries_of_citizenship:
+                        if country is not None and country != 'null' and country in COUNTRY_LANGUAGES \
+                                and countries_of_citizenship[country] >= OCCURRENCE_THRESHOLD_CITIZENSHIP \
+                                and country not in countries:
+                            countries.append(country)
 
                     if language == 'Multiple languages':
-                        languages_countries_of_birth = [
-                            l for c in countries_of_birth
-                            if c in COUNTRY_LANGUAGES
-                            for l in COUNTRY_LANGUAGES[c]
-                        ]
+                        continue
 
-                    if languages_countries_of_birth:
-                        for l in languages_countries_of_birth:
-                            if Language.get(l).language_name() in entity['name'].keys():
-                                continue
-                            anthroponym = {
-                                'name_romanised': name_romanised,
-                                'name': name['name'],
-                                'language': Language.get(l).language_name(),
-                                'language_code': l,
-                                'id': entity['id'],
-                                'type': entity['type'],
-                                'country': countries,
-                            }
-                            output.write(json.dumps(anthroponym) + '\n')
-                            counter += 1
-                            character_counter.update(name_romanised)
-                            breakdown_by_language.update([Language.get(l).language_name()])
-                            breakdown_by_length[length] += 1
+                    anthroponym = {
+                        'name_romanised': name_romanised,
+                        'name': name['name'],
+                        'language': language,
+                        'language_code': name['code'],
+                        'id': entity['id'],
+                        'type': entity['type'],
+                        'country': countries,
+                    }
+                    output.write(json.dumps(anthroponym) + '\n')
+                    counter += 1
+                    character_counter.update(name_romanised)
+                    breakdown_by_language.update([language])
+                    breakdown_by_length[length] += 1
 
-                            for country in anthroponym['country']:
-                                breakdown_by_country.update([country])
-
-                    elif language != 'Multiple languages':
-                        anthroponym = {
-                            'name_romanised': name_romanised,
-                            'name': name['name'],
-                            'language': language,
-                            'language_code': name['code'],
-                            'id': entity['id'],
-                            'type': entity['type'],
-                            'country': countries,
-                        }
-                        output.write(json.dumps(anthroponym) + '\n')
-                        counter += 1
-                        character_counter.update(name_romanised)
-                        breakdown_by_language.update([language])
-                        breakdown_by_length[length] += 1
-
-                        for country in anthroponym['country']:
-                            breakdown_by_country.update([country])
+                    for country in anthroponym['country']:
+                        breakdown_by_country.update([country])
 
     countries_id_names = get_countries_names(list(breakdown_by_country.keys()))
 
