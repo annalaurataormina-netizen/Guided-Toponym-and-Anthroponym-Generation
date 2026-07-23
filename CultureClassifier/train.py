2211487ec4dc5d84d18fc0abbc2a4bf45d51c26b
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-07-23 19:05:36 +0100

    Updated the code in CultureClassifier/train.py to be used on the ContrastiveVAE.

--- CultureClassifier/train.py
+++ CultureClassifier/train.py
@@ -8,11 +8,11 @@ from torch.utils.data import DataLoader
 
 from AE.CharVocab import CharVocab
 from AE.config import ALLOWED_CHARS
+from ContrastiveVAE.ContrastiveVAE import ContrastiveVAE
 from ContrastiveVAE.NameDataset import NameDataset
 from CultureClassifier.CultureClassifier import CultureClassifier
 from CultureClassifier.LatentDataset import LatentDataset
 from CultureClassifier.LatentExtractor import LatentExtractor
-from VAE.VAE import VAE
 from utils import load_all, normalise
 
 '''
@@ -33,13 +33,13 @@ def train():
     print(f"Using device: {device}")
 
     # ContrastiveVAE hyperparameters
-    '''
-    batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 64, 0.0015, 30, 0.005, 5
-    proj_hidden_dim, proj_output_dim, temperature, lambda_supcon = 128, 64, 0.07, 0.05
-    '''
+    batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 32, 64, 32, 2, 1, 64, 0.0015, 30, 0.005, 5
+    proj_hidden_dim, proj_output_dim, temperature, lambda_supcon = 128, 64, 0.1, 0.25
 
     # VAE hyperparameters
+    '''
     batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 64, 0.0015, 30, 0.005, 5
+    '''
 
     # Classifier hyperparameters
     hidden_dim, lr_classifier, epochs_classifier = 256, 0.001, 10
@@ -72,16 +72,16 @@ def train():
 
     # Recreate the model architecture first, then load the weights from the saved model
     # ContrastiveVAE
-    '''
     model = ContrastiveVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
                            num_layers_decoder, latent_dim, proj_hidden_dim, proj_output_dim)
     model_name = f'ContrastiveVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_phd{proj_hidden_dim}_pod{proj_output_dim}_t{temperature}_l{lambda_supcon}.pt'
-    '''
 
     # VAE
+    '''
     model = VAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder,
                 latent_dim)
     model_name = f'VAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}.pt'
+    '''
 
     print(f"Testing on {model_name}")
     print(f"Hidden dimension: {hidden_dim}")
