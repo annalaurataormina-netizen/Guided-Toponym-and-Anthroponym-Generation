86cc1f88f47d2fb27238da0d584a59c60d999ae1
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-07-24 11:59:04 +0100

    Updated the evaluate_latent_space.py script so that it can also be ran on the VAE.

--- ContrastiveVAE/evaluate_latent_space.py
+++ ContrastiveVAE/evaluate_latent_space.py
@@ -20,6 +20,11 @@ def evaluate_latent_space():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
 
+    # VAE hyperparameters
+    '''
+    batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 64, 0.0015, 30, 0.005, 5
+    '''
+
     # ContrastiveVAE hyperparameters
     batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 32, 64, 32, 2, 1, 64, 0.0015, 30, 0.005, 5
     proj_hidden_dim, proj_output_dim, temperature, lambda_supcon = 128, 64, 0.1, 0.25
@@ -42,19 +47,17 @@ def evaluate_latent_space():
     dataset = NameDataset(names_normalised, vocab)
 
     # Load model
-    model = ContrastiveVAE(
-        vocab,
-        embed_dim,
-        hidden_dim_encoder,
-        hidden_dim_decoder,
-        num_layers_encoder,
-        num_layers_decoder,
-        latent_dim,
-        proj_hidden_dim,
-        proj_output_dim
-    )
-
+    # ContrastiveVAE
+    model = ContrastiveVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
+                           num_layers_decoder, latent_dim, proj_hidden_dim, proj_output_dim)
     model_name = f'ContrastiveVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_phd{proj_hidden_dim}_pod{proj_output_dim}_t{temperature}_l{lambda_supcon}.pt'
+
+    # VAE
+    '''
+    model = VAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder,
+                latent_dim)
+    model_name = f'VAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}.pt'
+    '''
 
     print(model_name)
 
