aa6ea28bdf780b2a5aeabdd39144e07e56ad8a2b
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-07-28 12:08:17 +0100

    Fixed typos in the scripts.

--- scripts/generate_Conditional.sh
+++ scripts/generate_Conditional.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --out=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/ConditionalVAE/out/generata_%j.out
+#SBATCH --out=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/ConditionalVAE/out/generate_%j.out
 #SBATCH --error=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/ConditionalVAE/err/generate_%j.err
 
 cd /vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/
