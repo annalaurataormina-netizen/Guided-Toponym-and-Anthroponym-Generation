21abc0c28e072e4aefc3f1dce610cd30b1ffb0f2
Anna Taormina <at2225@imperial.ac.uk>
2026-08-04 13:39:52 +0100

    Adding the latest models to the repo

--- CultureClassifier/train.py
+++ CultureClassifier/train.py
@@ -32,7 +32,7 @@ def train():
     print(f"Using device: {device}")
 
     # Classifier hyperparameters
-    batch_size, embed_dim, hidden_dim, num_layers, lr, epochs = 512, 32, 64, 1, 0.001, 30
+    batch_size, embed_dim, hidden_dim, num_layers, lr, epochs = 512, 64, 256, 1, 0.001, 30
 
     # Vocabulary of characters
     vocab = CharVocab(ALLOWED_CHARS)
--- scripts/train_CultureClassifier.sh
+++ scripts/train_CultureClassifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --out=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/LatentCultureClassifier/out/train_%j.out
-#SBATCH --error=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/LatentCultureClassifier/err/train_%j.err
+#SBATCH --out=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/CultureClassifier/out/train_%j.out
+#SBATCH --error=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/CultureClassifier/err/train_%j.err
 
 cd /vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/
 python3 -m CultureClassifier.train
