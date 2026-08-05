c40ef90d8664b6319a55a78f1040c92f34a39116
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-05 12:57:10 +0100

    Fixed couple more bugs.

--- ContrastiveVAE2/compute_culture_stats.py
+++ ContrastiveVAE2/compute_culture_stats.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     model = VAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder,
                 latent_dim)
 
-    model_name = f'ContrastiveVAE2/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}.pt'
+    model_name = f'ContrastiveVAE2/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
 
     model.load_state_dict(torch.load(model_name, map_location=device))
 
