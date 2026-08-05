b0bdb47657a5efb41359ebe79b96c91ae2b807f0
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-05 14:20:44 +0100

    Fixed a bug.

--- evaluate_cultural_coherence.py
+++ evaluate_cultural_coherence.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
     # n_cycles, ratio = 4, 0.5
     proj_hidden_dim, proj_output_dim, temperature, lambda_supcon = 256, 128, 0.1, 0.75
     generator = ContrastiveVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
-                               num_layers_decoder, latent_dim, proj_hidden_dim, proj_output_dim)
+                               num_layers_decoder, latent_dim, proj_hidden_dim, proj_output_dim, culture_stats_path="ContrastiveVAE/culture_stats.pt")
     generator_name = f'ContrastiveVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_phd{proj_hidden_dim}_pod{proj_output_dim}_t{temperature}_l{lambda_supcon}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
 
