9f9d5257623d38c7e76f97f1f061f1b5e15bceae
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-06 10:51:27 +0100

    Trying to run the script to evaluate cultural coherence on the ConditionalVAE with larger cultural embedding size.

--- evaluate_cultural_coherence.py
+++ evaluate_cultural_coherence.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 
 from AE.CharVocab import CharVocab
 from AE.config import ALLOWED_CHARS
+from ConditionalVAE.ConditionalVAE import ConditionalVAE
 from ContrastiveVAE.ContrastiveVAE import ContrastiveVAE
 from ContrastiveVAE.NameDataset import NameDataset
 from CultureClassifier.CultureClassifier import CultureClassifier
@@ -185,6 +186,7 @@ if __name__ == "__main__":
     '''
 
     # ContrastiveVAE
+    '''
     batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 128, 0.0015, 100, 0.005, 5
     # free_bits = 0.05
     # n_cycles, ratio = 4, 0.5
@@ -194,19 +196,18 @@ if __name__ == "__main__":
                                culture_stats_path="ContrastiveVAE/culture_stats.pt")
     generator_name = f'ContrastiveVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_phd{proj_hidden_dim}_pod{proj_output_dim}_t{temperature}_l{lambda_supcon}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
+    '''
 
     # ConditionalVAE
-    '''
     batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 64, 0.0015, 100, 0.005, 5
     # free_bits = 0.05
     # n_cycles, ratio = 4, 0.5
-    culture_embed_dim = 16
+    culture_embed_dim = 64
     generator = ConditionalVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
                                num_layers_decoder, latent_dim, num_cultures, culture_embed_dim)
     generator_name = f'ConditionalVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_ced{culture_embed_dim}.pt'
     checkpoint = torch.load(generator_name, map_location=device)
     generator.load_state_dict(checkpoint["model_state_dict"])
-    '''
 
     print(f"Generator name: {generator_name}")
 
