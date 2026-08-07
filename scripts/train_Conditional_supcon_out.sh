7ee527e80b5d4dd64b9217674c19c395dea3e5a6
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-07 14:56:52 +0100

    Added to the repo the bash scripts to run the new scripts.

---
+++ scripts/train_Conditional_supcon_out.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --out=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/ConditionalVAE/out/train_supcon_out_%j.out
+#SBATCH --error=/vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/ConditionalVAE/err/train_supcon_out_%j.err
+
+cd /vol/bitbucket/at2225/Guided-Toponym-and-Anthroponym-Generation/
+python3 -m ConditionalVAE.train_supcon_out
