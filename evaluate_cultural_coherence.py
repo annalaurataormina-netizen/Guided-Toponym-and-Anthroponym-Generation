cab86e98bc0f64f77bae46f65c200df31af2bffd
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-09 14:22:22 +0100

    Script ready to evaluate model that uses the ad-hoc Sampler.

--- evaluate_cultural_coherence.py
+++ evaluate_cultural_coherence.py
@@ -202,8 +202,8 @@ if __name__ == "__main__":
     # free_bits = 0.05
     # n_cycles, ratio = 3, 0.5
     culture_embed_dim = 64
-    # temperature, lambda_supcon = 0.1, 0.75
-    margin, lambda_triplet = 1.0, 0.75
+    temperature, lambda_supcon = 0.1, 0.75
+    # margin, lambda_triplet = 1.0, 0.75
     generator = ConditionalVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
                                num_layers_decoder, latent_dim, num_cultures, culture_embed_dim)
     '''
@@ -230,8 +230,10 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_triplet_mu_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_m{margin}_l{lambda_triplet}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
     '''
+    '''
     generator_name = f'ConditionalVAE/models/best_model_triplet_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_m{margin}_l{lambda_triplet}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
+    '''
     '''
     generator_name = f'ConditionalVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_ced{culture_embed_dim}_se.pt'
     checkpoint = torch.load(generator_name, map_location=device)
@@ -240,8 +242,12 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_ced{culture_embed_dim}_se_cd.pt'
     checkpoint = torch.load(generator_name, map_location=device)
     '''
+    generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_s.pt'
+    checkpoint = torch.load(generator_name, map_location=device)
 
     print(f"Generator name: {generator_name}")
+
+    'best_model_supcon_out_bs512_ed64_hde64_hdd32_nle2_nld1_ld32_lr0.0015_ep100_blf0t0.005_t0.1_l0.75_s.pt'
 
     generator.to(device)
 
