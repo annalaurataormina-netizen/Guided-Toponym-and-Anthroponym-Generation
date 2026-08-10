5a0ff293eb521b530e5dd8e3b52dcc1e59719142
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-10 11:33:39 +0100

    Evaluate script to run on model with balanced sampler.

--- evaluate_cultural_coherence.py
+++ evaluate_cultural_coherence.py
@@ -226,6 +226,8 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
     '''
+    generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_s.pt'
+    generator.load_state_dict(torch.load(generator_name, map_location=device))
     '''
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_cd.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
@@ -254,8 +256,10 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_s.pt'
     checkpoint = torch.load(generator_name, map_location=device)
     '''
+    '''
     generator_name = f'ConditionalVAE/models/best_model_supcon_logits_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
     checkpoint = torch.load(generator_name, map_location=device)
+    '''
 
     print(f"Generator name: {generator_name}")
 
