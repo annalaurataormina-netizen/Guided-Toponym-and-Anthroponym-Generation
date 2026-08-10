e9569c17a639d07a78b461e2cb2d4d09661606ae
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-10 12:40:14 +0100

    Trying to use the last valid timestep..

--- ConditionalVAE/train_supcon_out.py
+++ ConditionalVAE/train_supcon_out.py
@@ -103,10 +103,10 @@ def train():
     '''
     model_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_bcf0t{beta_max}o{n_cycles}w{ratio}_t{temperature}_l{lambda_supcon}.pt'
     '''
+    '''
     model_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_mmp.pt'
     '''
     model_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_lvds.pt'
-    '''
 
     print(model_name)
 
@@ -265,15 +265,15 @@ def train():
             '''
 
             # SupCon loss (mean pooling with mask)
+            '''
             valid_lengths = (lengths - 1).to(decoder_hidden.device)
             mask = (torch.arange(decoder_hidden.size(1), device=decoder_hidden.device).unsqueeze(0) < valid_lengths.unsqueeze(1))
             decoder_embedding = (decoder_hidden * mask.unsqueeze(-1)).sum(dim=1) / valid_lengths.unsqueeze(1)
+            '''
 
             # SupCon loss (last timestep)
-            '''
             last_indices = (lengths - 1).to(decoder_hidden.device)
             decoder_embedding = decoder_hidden[torch.arange(decoder_hidden.size(0), device=decoder_hidden.device), last_indices]
-            '''
 
             decoder_embedding = F.normalize(decoder_embedding, dim=1)
             supcon_loss = supcon_criterion(decoder_embedding.unsqueeze(1), labels)
@@ -341,17 +341,15 @@ def train():
                         '''
 
                         # SupCon loss (mean pooling with mask)
+                        '''
                         valid_lengths = (lengths - 1).to(decoder_hidden.device)
-                        mask = (torch.arange(decoder_hidden.size(1), device=decoder_hidden.device).unsqueeze(
-                            0) < valid_lengths.unsqueeze(1))
-                        decoder_embedding = (decoder_hidden * mask.unsqueeze(-1)).sum(dim=1) / valid_lengths.unsqueeze(
-                            1)
+                        mask = (torch.arange(decoder_hidden.size(1), device=decoder_hidden.device).unsqueeze(0) < valid_lengths.unsqueeze(1))
+                        decoder_embedding = (decoder_hidden * mask.unsqueeze(-1)).sum(dim=1) / valid_lengths.unsqueeze(1)
+                        '''
 
                         # SupCon loss (last timestep)
-                        '''
                         last_indices = (lengths - 1).to(decoder_hidden.device)
                         decoder_embedding = decoder_hidden[torch.arange(decoder_hidden.size(0), device=decoder_hidden.device), last_indices]
-                        '''
 
                         decoder_embedding = F.normalize(decoder_embedding, dim=1)
                         supcon_loss = supcon_criterion(decoder_embedding.unsqueeze(1), labels)
@@ -482,10 +480,10 @@ def train():
     '''
     base_fig_name = f'loss_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_bcf0t{beta_max}o{n_cycles}w{ratio}_t{temperature}_l{lambda_supcon}'
     '''
+    '''
     base_fig_name = f'loss_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_mmp'
     '''
     base_fig_name = f'loss_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_lvds'
-    '''
 
     plt.figure(figsize=(8, 5))
     plt.plot(train_steps, train_losses, label="Training")
