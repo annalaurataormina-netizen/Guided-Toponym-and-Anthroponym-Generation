638dad8dd2a01e5d3a0aa55d2909f5690ea88981
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-11 10:28:24 +0100

    Evaluating model with logits.

--- evaluate_cultural_coherence.py
+++ evaluate_cultural_coherence.py
@@ -237,8 +237,10 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_mmp.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
     '''
+    '''
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_cd.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
+    '''
     '''
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_bcf0t{beta_max}o{n_cycles}w{ratio}_t{temperature}_l{lambda_supcon}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
@@ -263,10 +265,8 @@ if __name__ == "__main__":
     generator_name = f'ConditionalVAE/models/best_model_supcon_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}_s.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
     '''
-    '''
     generator_name = f'ConditionalVAE/models/best_model_supcon_logits_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
     generator.load_state_dict(torch.load(generator_name, map_location=device))
-    '''
 
     # ConditionalVAE2
     '''
