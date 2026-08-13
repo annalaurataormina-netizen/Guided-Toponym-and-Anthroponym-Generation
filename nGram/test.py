d10ca739c5323fba19bcfd2cce94fee9ab8d0a0b
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-13 10:54:30 +0100

    Added script to test nGram.

--- nGram/test.py
+++ nGram/test.py
@@ -30,7 +30,7 @@ def test():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    generator_name = f'ConditionalVAE/models/best_model_logits_out_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
+    generator_name = f'ConditionalVAE/models/best_model_logits_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
     generator = ConditionalVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
                            num_layers_decoder, latent_dim, num_cultures, culture_embed_dim)
     generator.load_state_dict(torch.load(generator_name, map_location=device))
