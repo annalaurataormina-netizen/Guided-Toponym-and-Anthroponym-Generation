6b209233929b37082bdcee23974c66856588e552
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-15 21:58:05 +0100

    Testing generation of the model with 0.025 / 0.75. Traditional generation.

--- nGram/test.py
+++ nGram/test.py
@@ -12,7 +12,7 @@ from utils import load_all
 
 def test():
 
-    batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 32, 0.0015, 100, 0.010, 5
+    batch_size, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder, num_layers_decoder, latent_dim, lr, epochs, beta_max, n_epochs_ramp_up = 512, 64, 64, 32, 2, 1, 32, 0.0015, 100, 0.025, 5
     culture_embed_dim = 64
     temperature, lambda_supcon = 0.1, 0.75
 
@@ -27,7 +27,7 @@ def test():
 
     generator_name = f'ConditionalVAE/models/best_model_supcon_logits_bs{batch_size}_ed{embed_dim}_hde{hidden_dim_encoder}_hdd{hidden_dim_decoder}_nle{num_layers_encoder}_nld{num_layers_decoder}_ld{latent_dim}_lr{lr}_ep{epochs}_blf0t{beta_max}_t{temperature}_l{lambda_supcon}.pt'
     generator = ConditionalVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
-                           num_layers_decoder, latent_dim, num_cultures, culture_embed_dim, culture_stats_path=f"ConditionalVAE/culture_stats_{beta_max}.pt")
+                           num_layers_decoder, latent_dim, num_cultures, culture_embed_dim)
     generator.load_state_dict(torch.load(generator_name, map_location=device))
 
     print(f"Generator name: {generator_name}")
