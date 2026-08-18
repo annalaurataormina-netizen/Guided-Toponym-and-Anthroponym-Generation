947f556661b890d28c1f021bb035d3ee54b642ab
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-18 10:35:38 +0100

    Changed scripts to train and evaluate the last model of the grid search.

--- CCVAE/grid_search.py
+++ CCVAE/grid_search.py
@@ -16,7 +16,6 @@ def grid_search():
     args = parser.parse_args()
     lr = args.lr
 
-    '''
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     vocab = CharVocab(ALLOWED_CHARS)
@@ -63,9 +62,8 @@ def grid_search():
     model = ConditionalVAE(vocab, embed_dim, hidden_dim_encoder, hidden_dim_decoder, num_layers_encoder,
                            num_layers_decoder, latent_dim, num_cultures, culture_embed_dim)
     model.load_state_dict(torch.load(model_name, map_location=device))
-    '''
 
-    model, train_names = train(lr)
+    # model, train_names = train(lr)
 
     evaluate(model, lr)
 
