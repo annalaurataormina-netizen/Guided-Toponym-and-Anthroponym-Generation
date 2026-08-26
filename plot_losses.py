ae3ee955ebc245050581a912e6f0532aa343c256
Anna Laura Taormina <annalaurataormina@Annas-MacBook-Pro.local>
2026-08-26 13:49:33 +0100

    Changed script to parse trainign data.

--- plot_losses.py
+++ plot_losses.py
@@ -15,20 +15,25 @@ def plot():
     axes[0].plot(df["Step"] / 1000, df["Train Recon"], label="Training")
     axes[0].plot(df["Step"] / 1000, df["Val Recon"], label="Validation")
     axes[0].set_ylabel("Reconstruction loss")
-    axes[0].legend()
 
     # Weighted KL
-    axes[1].plot(df["Step"] / 1000, df["Train β-KL"], label="Training")
-    axes[1].plot(df["Step"] / 1000, df["Val β-KL"], label="Validation")
-    axes[1].set_ylabel(r"$\beta$ KL divergence")
-    axes[1].legend()
+    axes[1].plot(df["Step"] / 1000, df["Train KL"], label="Training")
+    axes[1].plot(df["Step"] / 1000, df["Val KL"], label="Validation")
+    axes[1].set_ylabel(r"KL divergence")
 
     # SupCon
     axes[2].plot(df["Step"] / 1000, df["Train SupCon"], label="Training")
     axes[2].plot(df["Step"] / 1000, df["Val SupCon"], label="Validation")
     axes[2].set_ylabel("SupCon loss")
     axes[2].set_xlabel("Training step (×1000)")
-    axes[2].legend()
+
+    fig.legend(
+        ["Training", "Validation"],
+        loc="upper center",
+        bbox_to_anchor=(0.5, 0.93),
+        ncol=2,
+        frameon=False
+    )
 
     # Same ticks across all three plots
     ticks = np.arange(0, 451, 50)
@@ -38,7 +43,17 @@ def plot():
     axes[0].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
     axes[1].tick_params(axis="x", which="both", bottom=False, labelbottom=False)
 
-    plt.tight_layout()
+    for ax in axes:
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+
+    axes[0].spines["bottom"].set_visible(False)
+    axes[1].spines["top"].set_visible(False)
+    axes[1].spines["bottom"].set_visible(False)
+    axes[2].spines["top"].set_visible(False)
+
+    fig.subplots_adjust(hspace=0)
+    fig.align_ylabels(axes)
     plt.savefig("training_objectives.png", dpi=300, bbox_inches="tight")
     plt.show()
 
